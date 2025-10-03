630bc206913e9636266081fef7d9ebae8fbae153
Hee <93554130+hen3370@users.noreply.github.com>
2025-10-03 13:31:01 +0900

    commit2

--- src/test/java/com/labradorlabs/heyoung_ui/runners/CukesRunner.java
+++ src/test/java/com/labradorlabs/heyoung_ui/runners/CukesRunner.java
@@ -15,7 +15,7 @@ import org.junit.runner.RunWith;
         features = "src/test/resources/features",
         glue = "com/labradorlabs/heyoung_ui/steps_definitions",
         dryRun = false,
-        tags = "@US2",
+        tags = "",
         publish = true
 )
 
